3ed7f630c3e9843b40f043c9f74b09dd5b3d08c8
KatieB5 <katie.bickford2@gmail.com>
2024-02-19 14:38:35 +0000

    completed task 3 and associated test

--- controllers/topics_controller.js
+++ controllers/topics_controller.js
@@ -1,8 +1,16 @@
-const {selectAllTopics} = require('../models/topics_model');
+const {selectAllTopics, readAllEndpoints} = require('../models/topics_model');
 
 exports.getAllTopics = function getAllTopics(request, response, next) {
     selectAllTopics().then((topicsArr) => {
         response.status(200).send({topicsArr});
+    }).catch((error) => {
+        next(error);
+    });
+};
+
+exports.getAllEndpoints = function getAllEndpoints(request, response, next) {
+    readAllEndpoints().then((endpoints) => {
+        response.status(200).send({endpoints});
     }).catch((error) => {
         next(error);
     });
--- models/topics_model.js
+++ models/topics_model.js
@@ -1,7 +1,16 @@
 const db = require('../db/connection');
+const fs = require('fs/promises');
 
 exports.selectAllTopics = function selectAllTopics() {
     return db.query('SELECT * FROM topics').then((response) => {
         return response.rows;
+    });
+};
+
+exports.readAllEndpoints = function readAllEndpoints() {
+    return fs.readFile('./endpoints.json', 'utf8')
+    .then((response) => {
+        const parsedEndpoints = JSON.parse(response);
+        return parsedEndpoints;
     });
 };
