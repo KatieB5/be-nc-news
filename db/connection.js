c4a25ebdc360a76546f68c1d2511f9f164a74d61
KatieB5 <katie.bickford2@gmail.com>
2024-02-22 14:28:23 +0000

    modified connection file

--- db/connection.js
+++ db/connection.js
@@ -9,9 +9,11 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
+const config = {};
+
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
