c98ab2f447348d9aa65e35199e3d54d5462ec4b2
KatieB5 <katie.bickford2@gmail.com>
2024-03-12 09:31:22 +0000

    added article body to selectArticleById sql query

--- models/articles_model.js
+++ models/articles_model.js
@@ -38,7 +38,7 @@ exports.selectAllArticles = (topic, sort_by='created_at', order='DESC') => {
 };
 
 exports.selectArticleById = (article_id) => {
-    return db.query('SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id', [article_id])
+    return db.query('SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id', [article_id])
     .then(({rows}) => {
         if (rows.length === 0) {
             return Promise.reject({status: 404, msg: `No article found for article_id: ${article_id}`})
